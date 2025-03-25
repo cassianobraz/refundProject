d6c5753b6cbd87d048309914ff024932ba2b7c5c
Cassiano pereira braz <cassiano.braz@PORTOVELHO.RO.GOV.BR>
2025-03-25 10:43:01 -0400

    feat layout employee and route

---
+++ src/components/AppLayout.tsx
@@ -0,0 +1,11 @@
+import { Outlet } from "react-router";
+
+export function AppLayout() {
+  return (
+    <div className='w-screen h-screen flex flex-col items-center text-gray-100 bg-gray-400'>
+      <main className='p-3 w-full md:w-auto'>
+        <Outlet />
+      </main>
+    </div>
+  )
+}
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,12 @@
 import { BrowserRouter } from "react-router";
 
 import { AuthRoutes } from "./authRoutes";
+import { EmployeeRoutes } from "./EmployeeRoutes";
 
 export function Routes() {
   return (
     <BrowserRouter>
-      <AuthRoutes />
+      <EmployeeRoutes />
     </BrowserRouter>
   )
 }
